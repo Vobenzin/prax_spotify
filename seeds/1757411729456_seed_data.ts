01f97831ac8ea246e0c7a71724e35334a6179db2
ahi <ah>
2025-09-23 09:13:17 +0200

    test

--- seeds/1757411729456_seed_data.ts
+++ seeds/1757411729456_seed_data.ts
@@ -6,7 +6,7 @@ export async function seed(db: Kysely<DB>): Promise<void> {
   await db.deleteFrom("albums").execute();
   await db.deleteFrom("authors").execute();
 
-  for (let i = 0; i < 10; i += 1) {
+  for (let i = 0; i < 20; i += 1) {
     await db
       .insertInto("authors")
       .values({
@@ -18,14 +18,45 @@ export async function seed(db: Kysely<DB>): Promise<void> {
 
   const authors = await db.selectFrom("authors").selectAll().execute();
 
+
   for (const author of authors) {
-    await db
-      .insertInto("albums")
-      .values({
-        author_id: author.id,
-        name: faker.music.album(),
-        release_date: faker.date.past().getTime(),
+    const numAlbums = faker.number.int({ min: 0, max: 10 });
+
+    for (let i = 0; i < numAlbums; i += 1) {
+      await db
+        .insertInto("albums")
+        .values({
+          author_id: author.id,
+          name: faker.music.album(),
+          release_date: faker.date.past().getTime(),
+        })
+        .execute();
+
+    }
+
+  }
+  const albums = await db.selectFrom("albums").selectAll().execute();
+
+  for(const album in albums){
+    const typeOfAlbum=faker.number.int({min:0,max:9})
+    let  numSongs;
+    if (typeOfAlbum<=2){
+      numSongs=1
+
+    } else if(typeOfAlbum<5){
+      numSongs=faker.number.int({min:4,max:7})
+    }else{
+      numSongs=faker.number.int({min:10,max:20})
+    }
+    for(let i=0; i<numSongs; i++){
+      await db.insertInto("songs").values({
+        album_id: 1,
+        name: faker.music.songName(),
+        duration: faker.number.int({min:80,max:240})
       })
       .execute();
+    }
+
   }
 }
+
